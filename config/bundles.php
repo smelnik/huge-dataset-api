b15d37ea4a87e74fe62032c68f826cf0fa528690
PHP Sergey M. <smelnik.means.me@gmail.com>
2025-09-23 21:28:09 +0300

    chore(HDA-3): add OpenAPI documentation

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/HugeDatasetController.php
+++ src/Controller/HugeDatasetController.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Dto\Person;
 use App\Enum\CacheStatus;
 use App\Service\HugeDatasetService;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,6 +18,45 @@ final class HugeDatasetController extends AbstractController
         private readonly HugeDatasetService $hugeDatasetService
     ) {}
 
+    #[OA\Get(
+        path: '/process-huge-dataset',
+        description: 'Generates or returns a cached huge dataset',
+        summary: 'Process huge dataset',
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Cache STALE, HIT or MISS',
+                headers: [
+                    new OA\Header(
+                        header: 'X-Cache-Status', // <-- обязательно
+                        description: 'Cache status',
+                        schema: new OA\Schema(type: 'string', enum: ['HIT','MISS','STALE','WARMING'])
+                    )
+                ],
+                content: new OA\JsonContent(
+                    type: 'array',
+                    items: new OA\Items(ref: new Model(type: Person::class))
+                )
+            ),
+            new OA\Response(
+                response: 202,
+                description: 'Cache is warming',
+                headers: [
+                    new OA\Header(
+                        header: 'X-Cache-Status', // <-- обязательно
+                        description: 'Cache status',
+                        schema: new OA\Schema(type: 'string', enum: ['HIT','MISS','STALE','WARMING'])
+                    )
+                ],
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: 'message', type: 'string')
+                    ],
+                    type: 'object'
+                )
+            )
+        ]
+    )]
     #[Route('/process-huge-dataset', name: 'process_huge_dataset', methods: ['GET'])]
     public function processHugeDataset(): JsonResponse
     {
